f2d188de912efa9add659fa9e5de5a14b7cbdf70
TAHEE <jangth0655@gmail.com>
2021-12-10 17:07:53 +0900

    update router-v2

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Coin from "./Coin";
 import Coins from "./Coins";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Coins />} />
         <Route path="/:coinId/*" element={<Coin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
